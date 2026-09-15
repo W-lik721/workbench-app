c7f9054a0ac1ceae91018c7c67b5184cc5de0882
W-lik721 <1667057579@qq.com>
2026-09-15 18:21:12 +0800

    deploy(site): sw.js

--- sw.js
+++ sw.js
@@ -1,10 +1,10 @@
 ﻿// 个人工作台 Service Worker - 离线可开、可安装到主屏幕
-const CACHE = "workbench-v88";
+const CACHE = "workbench-v89";
 const FILES = [
   "./index.html",
   "./styles.css?v=65",
   "./schedule.js?v=3",
-  "./app.js?v=72",
+  "./app.js?v=73",
   "./manifest.json",
   "./icon.svg",
   "./icon-192.png",
